d66c2880e238e0d731fbf6861bed37e447f02acc
Eduard92 <eduard.huchin@gmail.com>
2018-08-20 11:34:41 -0500

    Libreria Facebook
    Dirección de Libreria
    \xampp\htdocs\addons\shared_addons\libraries

---
+++ libraries/Facebook/config.php
@@ -0,0 +1,4 @@
+<?php
+//Facebook configuration
+$config['App_ID']      =   '1861649017192859';
+$config['App_Secret']  =   '075ed7081aa8452223c41483eefb37ff'; 
---
+++ libraries/FacebookSDK.php
@@ -0,0 +1,16 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed');
+
+date_default_timezone_set('UTC');
+
+
+require __DIR__ .'/Facebook/Facebook.php';
+
+require_once __DIR__ . '/Facebook/autoload.php';
+
+class FacebookSDK
+	{
+
+	    function __construct() 
+	    {
+	    }
+    }
